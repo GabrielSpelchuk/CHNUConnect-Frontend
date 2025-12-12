7e886bdf74dd8652b7a6738d547d32fdc99df4fb
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2025-12-12 16:02:34 +0200

    Added verification of JWT for login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Registration from "./components/Auth/Registration";
 import HomePage from "./components/HomePage";
 import AdminPanel from "./components/AdminPanel/AdminPanel";
 
-
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem("token"));
   const [role, setRole] = useState(localStorage.getItem("role"));
+
+  function isJwtValid(token) {
+  if (!token) return false;
+
+  try {
+    const parts = token.split(".");
+    if (parts.length !== 3) return false;
+
+    const payload = JSON.parse(atob(parts[1]));
+
+    if (payload.exp) {
+      const now = Date.now() / 1000;
+      if (payload.exp < now) return false;
+    }
+
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
 
   const handleLogin = (userRole) => {
     setLoggedIn(true);
@@ -22,6 +41,14 @@ export default function App() {
     setLoggedIn(false);
     setRole(null);
   };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!isJwtValid(token)) {
+      handleLogout();
+    }
+  }, []);
 
   return (
     <BrowserRouter>
